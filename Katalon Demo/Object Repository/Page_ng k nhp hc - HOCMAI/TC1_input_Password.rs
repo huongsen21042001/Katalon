9b96af92411329d307b5fc860b59cf1e755ec403
Huong Sen <Huong Sen@PNVL063.pn.com.vn>
2022-01-23 15:33:12 +0700

    Homework1_Katalon

---
+++ Katalon Demo/Object Repository/Page_ng k nhp hc - HOCMAI/TC1_input_Password.rs
@@ -0,0 +1,312 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>TC1_input_Password</name>
+   <tag></tag>
+   <elementGuidId>ec93c0d9-c3ba-4259-bb09-fbc7f49b53a4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//form[@id='signupforms']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#signupforms</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>form</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>signupforms</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>signupforms</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>method</name>
+      <type>Main</type>
+      <value>post</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>action</name>
+      <type>Main</type>
+      <value>register.php</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>novalidate</name>
+      <type>Main</type>
+      <value>novalidate</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            
+                        Tạo tài khoản của bạn
+            Học tập và giao lưu với hàng triệu học viên trên mọi miền đất nước. 
+            
+                
+                    
+                        Đăng ký qua Facebook
+                    
+                
+                
+                    
+                        Đăng ký qua Google
+                    
+                
+                
+                    
+                        Đăng ký qua Yahoo
+                    
+                
+                
+                    
+                        Đăng ký với Apple
+                    
+                
+            
+
+            
+             Hoặc 
+            
+
+            
+                Họ và tên chỉ bao gồm chữ (viết hoa, viết thường) và có ít nhất hai từ tách biệt.
+            
+            
+            
+                Địa chỉ Email đã được sử dụng. Vui lòng kiểm tra lại.
+            
+            
+            
+                
+            
+            
+            
+                
+                    
+                    
+                    
+                
+                
+                    
+                
+            
+            
+
+            
+                
+                    (*) Khi bấm vào đăng ký tài khoản, bạn chắc chắn đã đọc và đồng ý với Chính sách bảo mật, Điều khoản dịch vụ và chính sách tư vấn của HOCMAI.                
+            
+            
+            
+                
+            
+            
+            
+        </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;signupforms&quot;)</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//form[@id='signupforms']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='main']/div/form</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Đăng ký'])[1]/following::form[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Đăng nhập'])[1]/following::form[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/form</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//form[@id = 'signupforms' and (text() = '
+            
+                        Tạo tài khoản của bạn
+            Học tập và giao lưu với hàng triệu học viên trên mọi miền đất nước. 
+            
+                
+                    
+                        Đăng ký qua Facebook
+                    
+                
+                
+                    
+                        Đăng ký qua Google
+                    
+                
+                
+                    
+                        Đăng ký qua Yahoo
+                    
+                
+                
+                    
+                        Đăng ký với Apple
+                    
+                
+            
+
+            
+             Hoặc 
+            
+
+            
+                Họ và tên chỉ bao gồm chữ (viết hoa, viết thường) và có ít nhất hai từ tách biệt.
+            
+            
+            
+                Địa chỉ Email đã được sử dụng. Vui lòng kiểm tra lại.
+            
+            
+            
+                
+            
+            
+            
+                
+                    
+                    
+                    
+                
+                
+                    
+                
+            
+            
+
+            
+                
+                    (*) Khi bấm vào đăng ký tài khoản, bạn chắc chắn đã đọc và đồng ý với Chính sách bảo mật, Điều khoản dịch vụ và chính sách tư vấn của HOCMAI.                
+            
+            
+            
+                
+            
+            
+            
+        ' or . = '
+            
+                        Tạo tài khoản của bạn
+            Học tập và giao lưu với hàng triệu học viên trên mọi miền đất nước. 
+            
+                
+                    
+                        Đăng ký qua Facebook
+                    
+                
+                
+                    
+                        Đăng ký qua Google
+                    
+                
+                
+                    
+                        Đăng ký qua Yahoo
+                    
+                
+                
+                    
+                        Đăng ký với Apple
+                    
+                
+            
+
+            
+             Hoặc 
+            
+
+            
+                Họ và tên chỉ bao gồm chữ (viết hoa, viết thường) và có ít nhất hai từ tách biệt.
+            
+            
+            
+                Địa chỉ Email đã được sử dụng. Vui lòng kiểm tra lại.
+            
+            
+            
+                
+            
+            
+            
+                
+                    
+                    
+                    
+                
+                
+                    
+                
+            
+            
+
+            
+                
+                    (*) Khi bấm vào đăng ký tài khoản, bạn chắc chắn đã đọc và đồng ý với Chính sách bảo mật, Điều khoản dịch vụ và chính sách tư vấn của HOCMAI.                
+            
+            
+            
+                
+            
+            
+            
+        ')]</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Katalon Demo/Object Repository/Page_ng k nhp hc - HOCMAI/TC6_button_Sigin.rs
@@ -0,0 +1,449 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>TC6_button_Sigin</name>
+   <tag></tag>
+   <elementGuidId>6c1dcffe-e433-4e92-a732-1d95eaf88a8c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//form[@id='signupforms']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#signupforms</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>form</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>signupforms</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>signupforms</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>method</name>
+      <type>Main</type>
+      <value>post</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>action</name>
+      <type>Main</type>
+      <value>register.php</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>novalidate</name>
+      <type>Main</type>
+      <value>novalidate</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            
+                        Tạo tài khoản của bạn
+            Học tập và giao lưu với hàng triệu học viên trên mọi miền đất nước. 
+            
+                
+                    
+                        Đăng ký qua Facebook
+                    
+                
+                
+                    
+                        Đăng ký qua Google
+                    
+                
+                
+                    
+                        Đăng ký qua Yahoo
+                    
+                
+                
+                    
+                        Đăng ký với Apple
+                    
+                
+            
+
+            
+             Hoặc 
+            
+
+            
+                
+            
+            
+            
+                
+            
+            
+            
+                
+            
+            
+            
+                
+                    
+                    
+                    
+                
+                
+                    Xác nhận mật khẩu không khớp. Vui lòng nhập lại.
+                
+            
+            
+
+            
+                
+                    (*) Khi bấm vào đăng ký tài khoản, bạn chắc chắn đã đọc và đồng ý với Chính sách bảo mật, Điều khoản dịch vụ và chính sách tư vấn của HOCMAI.                
+            
+            
+            
+                
+            
+            
+            
+        </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;signupforms&quot;)</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//form[@id='signupforms']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='main']/div/form</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Đăng ký'])[1]/following::form[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Đăng nhập'])[1]/following::form[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/form</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//form[@id = 'signupforms' and (text() = '
+            
+                        Tạo tài khoản của bạn
+            Học tập và giao lưu với hàng triệu học viên trên mọi miền đất nước. 
+            
+                
+                    
+                        Đăng ký qua Facebook
+                    
+                
+                
+                    
+                        Đăng ký qua Google
+                    
+                
+                
+                    
+                        Đăng ký qua Yahoo
+                    
+                
+                
+                    
+                        Đăng ký với Apple
+                    
+                
+            
+
+            
+             Hoặc 
+            
+
+            
+                
+            
+            
+            
+                
+            
+            
+            
+                
+            
+            
+            
+                
+                    
+                    
+                    
+                
+                
+                    Xác nhận mật khẩu không khớp. Vui lòng nhập lại.
+                
+            
+            
+
+            
+                
+                    (*) Khi bấm vào đăng ký tài khoản, bạn chắc chắn đã đọc và đồng ý với Chính sách bảo mật, Điều khoản dịch vụ và chính sách tư vấn của HOCMAI.                
+            
+            
+            
+                
+            
+            
+            
+        ' or . = '
+            
+                        Tạo tài khoản của bạn
+            Học tập và giao lưu với hàng triệu học viên trên mọi miền đất nước. 
+            
+                
+                    
+                        Đăng ký qua Facebook
+                    
+                
+                
+                    
+                        Đăng ký qua Google
+                    
+                
+                
+                    
+                        Đăng ký qua Yahoo
+                    
+                
+                
+                    
+                        Đăng ký với Apple
+                    
+                
+            
+
+            
+             Hoặc 
+            
+
+            
+                
+            
+            
+            
+                
+            
+            
+            
+                
+            
+            
+            
+                
+                    
+                    
+                    
+                
+                
+                    Xác nhận mật khẩu không khớp. Vui lòng nhập lại.
+                
+            
+            
+
+            
+                
+                    (*) Khi bấm vào đăng ký tài khoản, bạn chắc chắn đã đọc và đồng ý với Chính sách bảo mật, Điều khoản dịch vụ và chính sách tư vấn của HOCMAI.                
+            
+            
+            
+                
+            
+            
+            
+        ')]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//form[@id = 'signupforms' and (text() = '
+            
+                        Tạo tài khoản của bạn
+            Học tập và giao lưu với hàng triệu học viên trên mọi miền đất nước. 
+            
+                
+                    
+                        Đăng ký qua Facebook
+                    
+                
+                
+                    
+                        Đăng ký qua Google
+                    
+                
+                
+                    
+                        Đăng ký qua Yahoo
+                    
+                
+                
+                    
+                        Đăng ký với Apple
+                    
+                
+            
+
+            
+             Hoặc 
+            
+
+            
+                
+            
+            
+            
+                Định dạng email không hợp lệ.Chúng tôi sẽ sử dụng địa chỉ Email này cho những việc như bảo mật tài khoản của bạn, gửi thông báo cho bạn. Vui lòng nhập chính xác địa chỉ email của bạn.
+            
+            
+            
+                
+            
+            
+            
+                
+                    
+                    
+                    
+                
+                
+                    
+                
+            
+            
+
+            
+                
+                    (*) Khi bấm vào đăng ký tài khoản, bạn chắc chắn đã đọc và đồng ý với Chính sách bảo mật, Điều khoản dịch vụ và chính sách tư vấn của HOCMAI.                
+            
+            
+            
+                
+            
+            
+            
+        ' or . = '
+            
+                        Tạo tài khoản của bạn
+            Học tập và giao lưu với hàng triệu học viên trên mọi miền đất nước. 
+            
+                
+                    
+                        Đăng ký qua Facebook
+                    
+                
+                
+                    
+                        Đăng ký qua Google
+                    
+                
+                
+                    
+                        Đăng ký qua Yahoo
+                    
+                
+                
+                    
+                        Đăng ký với Apple
+                    
+                
+            
+
+            
+             Hoặc 
+            
+
+            
+                
+            
+            
+            
+                Định dạng email không hợp lệ.Chúng tôi sẽ sử dụng địa chỉ Email này cho những việc như bảo mật tài khoản của bạn, gửi thông báo cho bạn. Vui lòng nhập chính xác địa chỉ email của bạn.
+            
+            
+            
+                
+            
+            
+            
+                
+                    
+                    
+                    
+                
+                
+                    
+                
+            
+            
+
+            
+                
+                    (*) Khi bấm vào đăng ký tài khoản, bạn chắc chắn đã đọc và đồng ý với Chính sách bảo mật, Điều khoản dịch vụ và chính sách tư vấn của HOCMAI.                
+            
+            
+            
+                
+            
+            
+            
+        ')]</value>
+   </webElementXpaths>
+</WebElementEntity>
